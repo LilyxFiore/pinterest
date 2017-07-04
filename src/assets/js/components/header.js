95ce0f97b4e0fb3c9c461036ea7ed450dfacfa32
Dana Franco <turquoise.ilg@hotmail.com>
2017-07-04 14:06:06 -0500

    button inicio

--- src/assets/js/components/header.js
+++ src/assets/js/components/header.js
@@ -1,9 +1,20 @@
 'use strict';
 
 const Header = () => {
-  const header =$('<header></header>');
-  const h1     =$('<h1>Header</h1>');
+  const header      = $('<header></header>');
+  const nav         = $('<nav>',{class:'nav'});
+  const ulPin       = $('<ul>',{class:'ulPin'});
+  const liPin       = $('<li>',{class:'liPin'});
+  const aPin        = $('<a>',{href:'#',class:'aPin'});
+  const pin         = $('<img>',{src:'images/pinterest-logo.png',style:'width:2.5%',class:'pin'});
+  const textPin     = $('<span>',{text:'Inicio',class:'textPin'});
 
- header.append(h1);
+
+   ulPin.append(liPin);
+   liPin.append(aPin);
+   aPin.append(pin);
+   aPin.append(textPin);
+   nav.append(ulPin);
+   header.append(nav);
  return header;
 }
