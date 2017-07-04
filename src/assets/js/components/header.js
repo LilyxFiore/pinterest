5087561ef34723a4201216db6f18510ae99ff8a9
Dana Franco <turquoise.ilg@hotmail.com>
2017-07-04 16:12:18 -0500

    input

--- src/assets/js/components/header.js
+++ src/assets/js/components/header.js
@@ -4,14 +4,18 @@ const Header = () => {
   const header      = $('<header></header>');
   const nav         = $('<nav>',{class:'nav'});
   const ulPin       = $('<ul>',{class:'ulPin'});
-  const liPin       = $('<li>',{class:'liPin'});
+  const liPin1      = $('<li>',{class:'liPin1'});
+  const liPin2      = $('<li>',{class:'liPin2'});
   const aPin        = $('<a>',{href:'#',class:'aPin'});
-  const pin         = $('<img>',{src:'images/pinterest-logo.png',style:'width:2.5%',class:'pin'});
+  const pin         = $('<img>',{src:'images/pinterest-logo.png',style:'width:30px; height:30px',class:'pin'});
   const textPin     = $('<span>',{text:'Inicio',class:'textPin'});
+  const inputPin    = $('<input>',{type:'text',placeholder:'Buscar'});
 
 
-   ulPin.append(liPin);
-   liPin.append(aPin);
+   ulPin.append(liPin1);
+   ulPin.append(liPin2);
+   liPin1.append(aPin);
+   liPin2.append(inputPin);
    aPin.append(pin);
    aPin.append(textPin);
    nav.append(ulPin);
