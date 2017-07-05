c4833141d6326a844b176cb78e1134fa0a26e52a
Maria <mariacutipa.gonzales@gmail.com>
2017-07-05 15:24:27 -0500

    modal view

--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -1,31 +1,41 @@
 const Modal = () => {
-    const contenedor = '<div class="modal__contenedor"></div>';
-    const contData = '<div class="modal__image"></div>';
-    
-    const note = '<div class="modal__note"><h2>'+'SOFFA magazine 11 TEASER / design travel food people home lifestyle'+'<h2></div>'
-    const imagen = '<img src="" class="modal__imagen"/>';
-    
+    const modal = '<div class="modal flex flex__justify-center';
+    const contentModal('<div class="modal__contentModal"></div>');
+
     const headerModal = '<div class="modal__header flex flex__justify-between"></div>';
-    const contentIcons = '<div class=""></div>';
+    const contentIcons = '<div class="modal__header--icon flex"></div>';
     const iconShare = '<a href="#" class="btn__icons"><i class="fa fa-share-alt" aria-hidden="true"></i></a>';
     const iconCheck = '<a href="#" class="btn__icons"><i class="fa fa-check" aria-hidden="true"></i></a>';
     const iconMore = '<a href="#" class="btn__icons"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></a>';
     const save = '<a href="#" class="btn__guardar"><i class="fa fa-thumb-tack" aria-hidden="true"></i><span class="btn--pad">Guardar</span></a>';
     
+    const note = '<div class="modal__note"><h2>'+'SOFFA magazine 11 TEASER / design travel food people home lifestyle'+'<h2></div>'
+    
+    const contImagen = '<div class="modal__pin"></div>';
+    const imagen = '<img id="imageModal"src="" class="modal__pin--imagen"/>';
+    
+    contImagen.append(imagen);
+    
     contentIcons.append(iconShare);
     contentIcons.append(iconCheck);
     contentIcons.append(iconMore);
+    contentIcons.append(save);
+    headerModal.append(contentIcons);
     
-    headerModal.append();
-    contData.append();
-    contenedor.append(contData);
+    contentModal.append(headerModal);
+    contentModal.append(note);
+    contentModal.append(contImagen);
+    
+    modal.append(contentModal);
             
-    return contenedor;
+    return modal;
 }
 
 
 $( _=>{
    /*MODAL VIEW*/
+    Modal();
+    
     const modal = $('#modal');
     
     modal.hide();
