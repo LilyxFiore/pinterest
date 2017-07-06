2ed87ea41340421abbffc185e1b3db3c8f86f339
Maria <mariacutipa.gonzales@gmail.com>
2017-07-05 20:49:25 -0500

    modal merge ok

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,11 +1,10 @@
 'use strict';
 
 /** section board js **/
-
 const token = "AeQ7ekzAKmhr7pvDPpa00Xn5XZctFM4mjWCOA41EItrZVaA9_AAAAAA";
 const urlBoard = "arabelyuska/web-ui";
 
-const wrapper = $('<id class="wrapper"></id>');
+const wrapper = $('<div class="wrapper"></div>');
 const board = $('<section class="board"></section>');
 
 $.getJSON('https://api.pinterest.com/v1/boards/' + urlBoard +'/pins/?access_token=' + token +'&fields=link%2Cnote%2Cimage%2Cboard', function (response){
@@ -22,52 +21,72 @@ function addImages(data) {
   boardItem.append(boardItemImage);
   boardItem.append(boardItemTitle);
 
+  boardItem.on('click', (e)=>{
+      showModal(data.image.original.url,data.note);
+  });
+  
   return boardItem;
 }
 
 
 const Modal = () => {
-    const contenedor = '<div class="modal__contenedor"></div>';
-    const contData = '<div class="modal__image"></div>';
+    const contenedor =  $('<div id="modal" class="modal flex flex__justify-between"></div>');
+    const contTotal = $('<div class="modal__contentModal"></div>');
+    const contData =  $('<div class="modal__pin"></div>');
     
-    const note = '<div class="modal__note"><h2>'+'SOFFA magazine 11 TEASER / design travel food people home lifestyle'+'<h2></div>'
-    const imagen = '<img src="" class="modal__imagen"/>';
+    const note =  $('<div"><h1 id="note" class="modal__header--fonts modal__note"></h1></div>');
+    const imagen =  $('<div class="modal__image"><img id="imageModal" src="" class="modal__image--border modal__pin--imagen"/></div>');
     
-    const headerModal = '<div class="modal__header flex flex__justify-between"></div>';
-    const contentIcons = '<div class=""></div>';
-    const iconShare = '<a href="#" class="btn__icons"><i class="fa fa-share-alt" aria-hidden="true"></i></a>';
-    const iconCheck = '<a href="#" class="btn__icons"><i class="fa fa-check" aria-hidden="true"></i></a>';
-    const iconMore = '<a href="#" class="btn__icons"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></a>';
-    const save = '<a href="#" class="btn__guardar"><i class="fa fa-thumb-tack" aria-hidden="true"></i><span class="btn--pad">Guardar</span></a>';
+    const headerModal =  $('<div class="modal__header flex flex__justify-between"></div>');
+    const iconShare =  $('<a href="#" class="btn__icons"><i class="fa fa-share-alt" aria-hidden="true"></i></a>');
+    const iconCheck =  $('<a href="#" class="btn__icons"><i class="fa fa-check" aria-hidden="true"></i></a>');
+    const iconMore =  $('<a href="#" class="btn__icons"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></a>');
+    const save =  $('<button class="btn__guardar modal__header--button"><i class="fa fa-thumb-tack" aria-hidden="true"></i><span class="btn--pad">Guardar</span></button>');
     
-    contentIcons.append(iconShare);
-    contentIcons.append(iconCheck);
-    contentIcons.append(iconMore);
-    headerModal.append(contentIcons);
-    headerModal.append(save);
+    const close =  $('<span id="close" class="modal__close"><i class="fa fa-times" aria-hidden="true"></i></span>'); 
+    const iconos =  $('<div class="modal--iconos"></div>');
+
+    iconos.append(iconShare);
+    iconos.append(iconCheck);
+    iconos.append(iconMore);
+    iconos.append(save);
+
+    headerModal.append(iconos);
     
-    contData.append();  
-    contenedor.append(contData);
+    contData.append(note);
+    contData.append(imagen);
+
+    contTotal.append(headerModal);
+    contTotal.append(contData);
+    
+    contenedor.append(close);
+    contenedor.append(contTotal);
+    
+    close.on('click', ()=>{
+      contenedor.hide();
+    });
+
             
     return contenedor;
 }
 
+
 $('#root').append(wrapper);
 wrapper.append(board);
-wrapper.append(Modal);
+$('#root').append(Modal());
 
-$( _=>{
-    /*MODAL VIEW*/
-    const modal = $('#modal');
-    /*render(modal);*/
-    modal.hide();
-    
-    $('.content img').on('click', (e)=>{
-        modal.show('fall');
-        $('#imageModal').attr('src', e.target.src);
-    });
+
+/*MODAL VIEW*/
+  const modal = $('#modal');
+  /*render(modal);*/
+  modal.hide();
   
-    $('#close').on('click', ()=>{
-     modal.hide(1000); 
-    });
-});
+
+  $('#modal').hide();
+
+  function showModal(src,data){
+      const modal = $('#modal');
+      modal.show();
+      $('#imageModal').attr('src', src);
+      $('#note').html(data);
+  }
